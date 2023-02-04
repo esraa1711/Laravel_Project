617d3dc7ec3d8eed4e33861430b8774002a1f55f
esraa1711 <esraakhaled163@gmail.com>
2023-02-04 10:27:05 +0200

    logout change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,8 +54,13 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
+<form id="logout-form" action="{{ route('logout') }}" method="POST" >
+    @csrf
+    {{-- < href="{{ route('logout') }}">logout</a> --}}
+    <input type="submit" value="logout">
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+</form>
+                                {{-- <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -65,7 +70,7 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
-                                </div>
+                                </div> --}}
                             </li>
                         @endguest
                     </ul>
